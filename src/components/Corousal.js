28e0cade55b910fceb3de0e98819483de599cb69
Harsh Johar <hpsj3112@gmail.com>
2022-04-23 14:15:03 +0530

    Components

---
+++ src/components/Corousal.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Corousal = () => {
+  return (
+    <div>Corousal</div>
+  )
+}
+
+export default Corousal
---
+++ src/components/ImportantDetails.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ImportantDetails = () => {
+  return (
+    <div>ImportantDetails</div>
+  )
+}
+
+export default ImportantDetails
