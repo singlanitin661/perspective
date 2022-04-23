97ffc31f09e1fcb9ebbb16f5c6f7aa12c71ff96a
Harsh Johar <hpsj3112@gmail.com>
2022-04-23 23:52:45 +0530

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,28 +1,40 @@
-import React, { useState } from 'react'
-import './Header.css'
-import {FaBars,FaTimes} from "react-icons/fa"
+import React, { useState } from "react";
+import "./Header.css";
+import { FaBars, FaTimes } from "react-icons/fa";
+import pecLogo from "../../assets/PERS.png";
 
 const Header = () => {
-  const [hamburger, setHamburger] = useState(false)
-  const toggle=()=>{
-    setHamburger(!hamburger)
-  }
-  const arr=["Home","Research","Gallery","FAQ","Awards"]
-  return (
-    <>
-    <div className="header-root">
+    const [hamburger, setHamburger] = useState(false);
+    const toggle = () => {
+        setHamburger(!hamburger);
+    };
+    const arr = ["Home", "Research", "Gallery", "FAQ", "Awards"];
+    return (
+        <>
+            <div className="header-root">
+                <div className="bars" onClick={toggle}>
+                    {!hamburger ? <FaBars /> : <FaTimes />}
+                </div>
+                <nav className="header-main">
+                  <img src={pecLogo} alt="persPECtive" className="header-logo"/>
+                    <ul className={`header-list` + (hamburger ? +"-ham" : "")}>
+                        {arr.map((item) => {
+                            return (
+                                <li
+                                    className={
+                                        `header-list-item` +
+                                        (hamburger ? +"-ham" : "")
+                                    }
+                                >
+                                    {item}
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </nav>
+            </div>
+        </>
+    );
+};
 
-    <div className='bars' onClick={toggle}>{!hamburger?<FaBars/>:<FaTimes/>}</div>
-    <nav className='header-main'>
-      <ul className={`header-list`+(hamburger?+'-ham':"")}>
-        {arr.map((item)=>{
-          return <li className={`header-list-item`+(hamburger?+"-ham":"")}>{item}</li>
-        })}
-      </ul>
-      </nav>
-    </div>
-    </>
-  )
-}
-
-export default Header
+export default Header;
